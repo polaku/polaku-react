efd3c7f8ea4df7bc501602141bc927eb9bf53b9f
fbryo21 <febryryo21@gmail.com>
2021-06-14 17:59:44 +0700

    UPDATE BaseURL

--- src/config/API.js
+++ src/config/API.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { browserName, osName, isMobile } from "react-device-detect";
 
-const BaseURL = 'http://localhost:4000';
-// const BaseURL = 'http://165.22.110.159'; //server digitalocean
+// const BaseURL = 'http://localhost:4000';
+const BaseURL = "http://165.22.110.159"; //server digitalocean
 // const BaseURL = 'http://api.polagroup.co.id';
 
 const API = axios.create({
@@ -12,7 +12,7 @@ const API = axios.create({
     os: osName,
     isMobile,
   },
-  timeout: 60000
-})
+  timeout: 60000,
+});
 
 export { API, BaseURL };
